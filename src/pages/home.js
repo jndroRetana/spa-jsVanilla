ae15f509d546d6994e26bf48f8187cecbdfcf137
jndro <jndro.retana@gmail.com>
2020-08-05 15:06:55 -0500

    Se crearon las configuraciones para la utilicación de webpack y babel (para usar javascript moderno), se crearon las vistas de los diferentes componentes de la página (home, character, header)

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+const Home = () => {
+  const view = `
+  <div class="characters">
+    <article class="character-item">
+      <a href="#/1/">
+        <img src="image" alt="imagen del personaje" />
+      </a>
+    </article>
+  </div>
+  `;
+  return view;
+};
+
+export default Home;
