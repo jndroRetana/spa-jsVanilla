5a01925fdc20455c31ed7355a05e8e4740ef7ba2
jndro <jndro.retana@gmail.com>
2020-08-05 19:12:39 -0500

    Se modificó la page de character para mostrar las caracteristicas específicas de cada personaje

--- src/pages/character.js
+++ src/pages/character.js
@@ -1,17 +1,22 @@
-const Character = () => {
+import getHash from "../utils/getHash";
+import getData from "../utils/getData";
+
+const Character = async () => {
+  const hash = getHash();
+  const data = await getData(hash);
   const view = `
     <div class="Characters-inner">
         <articleclass="Character-card">
-            <imgsrc="image"alt"name">
-            <h2>Name</h2>
+            <img src="${data.image}" alt="${data.name}">
+            <h2>${data.name}</h2>
         </article>
         <articleclass="Character-card">
-            <h3>Epsiodes:</h3>
-            <h3>Status:</h3>
-            <h3>Species:</h3>
-            <h3>Gender</h3>
-            <h3>Origin:</h3>
-            <h3>Last Location:</h3>
+            <h3>Epsiodes: ${data.episode.length}</h3>
+            <h3>Status: ${data.status}</h3>
+            <h3>Species: ${data.species}</h3>
+            <h3>Gender: ${data.gender}</h3>
+            <h3>Origin: ${data.origin.name}</h3>
+            <h3>Last Location: ${data.location.name}</h3>
         </article>
     </div>
   `;
